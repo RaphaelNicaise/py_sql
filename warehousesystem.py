6ddca30f4cff02c74af91057367281d40c0d8537
Raphael Nicaise <raphanicaise@gmail.com>
2023-12-14 04:11:33 -0300

    Update warehousesystem.py

--- warehousesystem.py
+++ warehousesystem.py
@@ -1,5 +1,7 @@
 import mysql.connector
 import random
+import pandas as pd
+
 cnx = mysql.connector.MySQLConnection(user='root',password='',host='localhost',
                                       database='warehousesystem',port='3306')
 cursor = cnx.cursor()
@@ -20,39 +22,43 @@ def p(text):
 
 
 while (True):
-    print("| WarehouseSystem |")
-    print("1 - Select a Product ID & Calculate Price \n2 - Insert N randon products \n3 - Quit")
-    choice = int(input("Choose an Option -> "))
-    if choice == 1:
-        id_product = input("Select an Id_product: ")
-        if id_product.lower() == 'quit':
-            p("Returning to menu")
+    try:
+        print("| WarehouseSystem |")
+        print("1 - Select a Product ID & Calculate Price \n2 - Insert N randon products \n3 - Quit")
+        choice = int(input("Choose an Option -> "))
+        if choice == 1:
+            id_product = input("Select an Id_product: ")
+            if id_product.lower() == 'quit':
+                p("Returning to menu")
+                break
+            id_product = int(id_product)
+            cursor.execute(f"select product_name from products where id_product = {id_product}")
+            productname = cursor.fetchone()[0]
+            q = input(f"How many of {productname}? -> ")
+            cursor.execute(f"Select price*{q} from products where id_product = {id_product} ")
+            product = cursor.fetchone()
+            price = product[0]
+            p(f"{q} of {productname} -> {price}$ ")   
+        elif choice == 2:
+            max = int(input("How Many random inserts do you want? -> "))
+            i = 0
+            while (i < max):
+                rand_product = choose_random_product()
+                rand_quantity = choose_random_quantity()
+                cursor.callproc("add_stock_without_text",(rand_product,rand_quantity))
+                cursor.execute(f"select product_name from products where id_product = {rand_product} ")
+                productname_ = cursor.fetchone()[0]  
+                if rand_product >= 10:
+                    p(f"Product: {rand_product}- {productname_} Added: {rand_quantity}")
+                elif rand_product < 10:
+                    p(f"Product: {rand_product}- {productname_} Added: {rand_quantity}")
+                i += 1
+            cnx.commit()
+        elif choice == 3:
+            p("Leaving program")
             break
-        id_product = int(id_product)
-        cursor.execute(f"select product_name from products where id_product = {id_product}")
-        productname = cursor.fetchone()[0]
-        q = input(f"How many of {productname}? -> ")
-        cursor.execute(f"Select price*{q} from products where id_product = {id_product} ")
-        product = cursor.fetchone()
-        price = product[0]
-        p(f"{q} of {productname} -> {price}$ ")   
-    elif choice == 2:
-        max = int(input("How Many random inserts do you want? -> "))
-        i = 0
-        while (i < max):
-            rand_product = choose_random_product()
-            rand_quantity = choose_random_quantity()
-            cursor.callproc("add_stock_without_text",(rand_product,rand_quantity))
-            cursor.execute(f"select product_name from products where id_product = {rand_product} ")
-            productname_ = cursor.fetchone()[0]  
-            p(f"Product: {rand_product}- {productname_} Added: {rand_quantity}")
-            i += 1
-        cnx.commit()
-    elif choice == 3:
-        p("Leaving program")
-        break
-    
-        
+    except ValueError:
+        print("Wrong character")
 cnx.close()
  
     
