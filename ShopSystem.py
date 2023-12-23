520ff0a9ef66f56ec4d6e4e2c35614dde7a18abb
Raphael Nicaise <raphanicaise@gmail.com>
2023-12-23 18:12:30 -0300

    Update ShopSystem.py

--- ShopSystem.py
+++ ShopSystem.py
@@ -2,7 +2,8 @@ import mysql.connector
 import pandas as pd
 
 cnx = mysql.connector.MySQLConnection(
-    ser='root',password='',host='localhost',
+    user='root',password='',host='localhost',
     database='warehousesystem',port='3306'
 )
 cursor = cnx.cursor()
+print("MySQL Connected")
