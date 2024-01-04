df4373eb6d68d4f64853931cd7320c3f172975ec
Raphael Nicaise <raphanicaise@gmail.com>
2024-01-04 08:23:02 -0300

    Update DB.sql

--- warehousesys/DB.sql
+++ warehousesys/DB.sql
@@ -97,3 +97,66 @@ BEGIN
     END IF;
     COMMIT;
 END
+
+CREATE TRIGGER ins_outs_stock 
+AFTER UPDATE ON inventory
+FOR EACH ROW
+BEGIN
+    DECLARE quantity_change INT;
+
+    SET quantity_change = NEW.quantity - OLD.quantity;
+	IF OLD.quantity != NEW.quantity THEN
+		IF quantity_change > 0 THEN
+			INSERT INTO stock_movements (id_product,old_quantity, quantity,type,new_quantity)
+			VALUES (OLD.id_product,OLD.quantity, quantity_change,'ins',NEW.quantity);
+			
+		ELSEIF quantity_change < 0 THEN
+			INSERT INTO stock_movements (id_product,old_quantity, quantity,type,new_quantity)
+			VALUES (OLD.id_product,OLD.quantity, -quantity_change,'outs',NEW.quantity);
+		END IF;
+	END IF;
+    
+END //
+
+DELIMITER //
+
+CREATE TRIGGER insert_product_into_inventory
+AFTER INSERT ON products
+FOR EACH ROW
+BEGIN
+    INSERT INTO inventory (id_product, quantity)
+    VALUES (NEW.id_product, 0);
+END //
+
+DELIMITER //
+
+CREATE TRIGGER update_price_of_product
+AFTER UPDATE ON products
+FOR EACH ROW
+BEGIN
+	IF OLD.price <> NEW.price THEN
+		INSERT INTO price_changes (id_product,old_price,new_price)
+		VALUES (new.id_product,OLD.price,NEW.price);
+	END IF;
+END //
+
+DELIMITER //
+CREATE TRIGGER alert_amount0_product
+AFTER UPDATE ON inventory
+FOR EACH ROW
+BEGIN
+	IF NEW.Quantity = 0 AND OLD.Quantity > 0 THEN
+		INSERT INTO alert_amount_0 (id_product,alert) 
+        VALUES (NEW.id_product, 'Add Stock!');
+	END IF;
+END //
+
+DELIMITER //
+CREATE TRIGGER delete_alert_when_stocked
+AFTER UPDATE ON inventory
+FOR EACH ROW
+BEGIN
+	IF OLD.Quantity = 0 AND NEW.Quantity > 0 THEN
+		DELETE FROM alert_amount_0 WHERE id_product = NEW.id_product;
+	END IF;
+END //
