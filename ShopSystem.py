076d0b27157afd7523d22de6e4ee3d3c682f264c
Rapha <raphanicaise@gmail.com>
2024-02-08 04:50:47 -0300

    Eliminados

--- ShopSystem.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-import pandas as pd
-
-cnx = mysql.connector.MySQLConnection(
-    user='root',password='',host='localhost',
-    database='warehousesystem',port='3306'
-)
-cursor = cnx.cursor()
-print("MySQL Connected")
